806f7d168a067ca19a32ac06901718e6c149d44e
joqwer <94891082+joqwer@users.noreply.github.com>
2023-07-16 16:05:33 -0400

    Added crud op to get user by user_id

--- serverapp/crud.py
+++ serverapp/crud.py
@@ -30,3 +30,6 @@ def create_user(db: Session) -> tuple[str, int]:
     db.commit()
 
     return password, new_user.user_id
+
+def get_user_by_id(db: Session, user_id: str) -> models.User | None:
+    return db.query(models.User).filter(models.User.user_id == user_id).one_or_none()
