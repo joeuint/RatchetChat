f47b6af0c81d42c0a21eaeffa47f54646d132eed
joqwer <94891082+joqwer@users.noreply.github.com>
2023-07-16 16:33:15 -0400

    New ConvoRequest model

--- database/models.py
+++ database/models.py
@@ -1,8 +1,14 @@
 from .database import Base
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 class User(Base):
     __tablename__ = 'users'
     user_id         = Column(String, primary_key=True, nullable=False)
     hashed_password = Column(String, nullable=False)
 
+class ConvoRequest(Base):
+    __tablename__ = 'convo_requests'
+    convo_request_id = Column(String, primary_key=True, nullable=False)
+    requester_id = Column(String, ForeignKey('users.user_id'), nullable=False)
+    target_id = Column(String, ForeignKey('users.user_id'), nullable=False)
