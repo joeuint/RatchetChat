1446e1b1ebf213483f8c796e5751c3692832f309
joqwer <94891082+joqwer@users.noreply.github.com>
2023-07-16 05:31:34 -0400

    Added account creation

---
+++ serverapp/crud.py
@@ -0,0 +1,32 @@
+from sqlalchemy.orm import Session
+from uuid import uuid4
+from . import models
+import string
+from secrets import SystemRandom
+from argon2 import PasswordHasher
+
+password_char_list = string.ascii_lowercase + string.ascii_uppercase + string.digits + '#$%&'
+
+secure_rng = SystemRandom()
+
+ph = PasswordHasher(
+    hash_len=32,
+    memory_cost=1024*1024,
+    parallelism=4,
+    time_cost=3,
+)
+
+def create_user(db: Session):
+    password = ''.join(secure_rng.choices(password_char_list, k=32))
+
+    hashed_password = ph.hash(password)
+
+    new_user = models.User(
+        user_id = str(uuid4()),
+        hashed_password = hashed_password,
+    )
+
+    db.add(new_user)
+    db.commit()
+
+    return password, new_user.user_id
--- serverapp/main.py
+++ serverapp/main.py
@@ -1,10 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 
 from cryptography.hazmat.primitives.asymmetric.x25519 import X25519PrivateKey
 from cryptography.hazmat.primitives.serialization import Encoding, PrivateFormat, NoEncryption, PublicFormat
 
+from sqlalchemy.orm import Session
+
 from .database import SessionLocal, engine
 from . import models
+from . import crud
 
 app = FastAPI()
 
@@ -26,4 +29,12 @@ def gen_keys():
     return {
                 'private': priv_key.private_bytes(Encoding.PEM, PrivateFormat.PKCS8, NoEncryption()),
                 'public': priv_key.public_key().public_bytes(Encoding.PEM, PublicFormat.SubjectPublicKeyInfo)
+            }
+
+@app.post('/accountcreate')
+def gen_account(db: Session = Depends(get_db)):
+    unhashed, user_id = crud.create_user(db)
+    return {
+                'id': user_id,
+                'password': unhashed,
             }
