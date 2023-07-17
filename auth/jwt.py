4ae1ce885845ba6a7464ead85a5ae57d65d444da
joqwer <94891082+joqwer@users.noreply.github.com>
2023-07-17 02:34:18 -0400

    Added expiration to jwt

--- auth/jwt.py
+++ auth/jwt.py
@@ -1,5 +1,7 @@
 import jwt
 import os
+from datetime import datetime
+import time
 
 if not os.path.exists('signingsecret.txt'):
     raise Exception('Please create a signingsecret.txt file in the auth folder and fill it with random characters.')
@@ -9,6 +11,6 @@ with open('sigingsecret.txt') as f:
     
 
 def generate_jwt(user_id: int) -> str:
-    token = jwt.encode({'user_id': user_id}, signing_secret, algorithm='HS512')
+    token = jwt.encode({'user_id': user_id, 'exp': time.mktime(datetime.now().timetuple()) + 1209600}, signing_secret, algorithm='HS512')
 
     return token
